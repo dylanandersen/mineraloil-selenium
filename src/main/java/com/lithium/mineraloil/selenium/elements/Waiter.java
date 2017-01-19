d2f02cc3b84db0cf511c14b404d8a9664006ae13
hugh.mcgowan <hugh.mcgowan@lithium.com>
2017-01-19 12:36:52 -0600

    increase timeout and unclaim first

--- src/main/java/com/lithium/mineraloil/selenium/elements/Waiter.java
+++ src/main/java/com/lithium/mineraloil/selenium/elements/Waiter.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriverException;
 class Waiter {
     public static final int STALE_ELEMENT_WAIT_MS = 500;
     public static final int INTERACT_WAIT_S = 5;
-    public static final int DISPLAY_WAIT_S = 20;
+    public static final int DISPLAY_WAIT_S = 60;
     private static ConditionFactory conditionFactory;
 
     public static ConditionFactory await() {
